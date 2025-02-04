e0431970309a2b7e8392fcf815d74d3eaa3b98b6
Pascal Obiwumma <pobiwumma@gmail.com>
2025-02-04 22:29:41 +0100

    Prepared to write the function

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstShareBtn = document.querySelector('.item3')
+const secondShareBtn = document.querySelector('.item6')
+const inactive = document.querySelector('.item')
+const active = document.querySelector('.hidden')
+function displayChange() {
+  
+}
+console.log(secondShareBtn)
