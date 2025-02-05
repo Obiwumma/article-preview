8904026327df83230d8cbf1278c2019134656419
Pascal Obiwumma <pobiwumma@gmail.com>
2025-02-05 21:21:18 +0100

    Created a funtion to display share links

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const firstShareBtn = document.querySelector('.item3')
 const secondShareBtn = document.querySelector('.item6')
 const inactive = document.querySelector('.item')
 const active = document.querySelector('.hidden')
+
 function displayChange() {
-  
+  inactive.style.display = 'none'
+  active.style.display = 'flex'
 }
+firstShareBtn.addEventListener('click', displayChange)
+// displayChange()
 console.log(secondShareBtn)
