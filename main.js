348773a39a509afc7aad635c7880c60347e3c3d8
Pascal Obiwumma <pobiwumma@gmail.com>
2025-02-12 09:33:49 +0100

    Remove event listener from first share button

--- main.js
+++ main.js
@@ -33,13 +33,16 @@ function onSmallScreen() {
   console.log("Now at 1023px or smaller");
   firstShareBtn.addEventListener('click', displayLinks);
   secondShareBtn.addEventListener('click', hideLinks);
+  firstShareBtn.removeEventListener("click", toggleLinks);
 }
 
 function onMediumScreen() {
   console.log("Now at 1024px or more");
- 
+  
   firstShareBtn.removeEventListener("click", toggleLinks);
   firstShareBtn.addEventListener("click", toggleLinks);
+  inactiveElement.style.display = 'grid';
+
 }
 
 function onScreenSize() {
