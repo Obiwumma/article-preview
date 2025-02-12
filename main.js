7a849864960ec75b4624fec32c888605ee8ba8af
Pascal Obiwumma <pobiwumma@gmail.com>
2025-02-13 00:30:01 +0100

    reset isClicked on both screen sizes

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ function toggleLinks () {
     bothLinks();
   } else {
     hideLinks();
+
   }
   isClicked = !isClicked;
 };
@@ -33,24 +34,32 @@ function toggleLinks () {
 function onSmallScreen() {
   console.log("Now at 1023px or smaller");
   hideLinks()
+  isClicked = false;
+
   firstShareBtn.addEventListener('click', displayLinks);
   secondShareBtn.addEventListener('click', hideLinks);
   firstShareBtn.removeEventListener("click", toggleLinks);
-  console.log(inactiveElement.style.display);
 }
 
 function onMediumScreen() {
   console.log("Now at 1024px or more");
-  inactiveElement.style.display = 'grid';
+  hideLinks();
+  isClicked = false;
+
+  firstShareBtn.removeEventListener("click", displayLinks);
+  secondShareBtn.removeEventListener("click", hideLinks);
   firstShareBtn.removeEventListener("click", toggleLinks);
+
   firstShareBtn.addEventListener("click", toggleLinks);
 
+  inactiveElement.style.display = 'grid';
+  
 }
 
 function onScreenSize() {
   if (window.matchMedia("(max-width: 1023px)").matches) {
     onSmallScreen();
-  } else if (window.matchMedia("(max-width: 1024px)").matches) {
+  } else if (window.matchMedia("(min-width: 1024px)").matches) {
     onMediumScreen();
   }
 }
